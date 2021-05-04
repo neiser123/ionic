2848389fb330b906026041951a0d7b1e0f64aa90
neiser <neisermontano1991@gmail.com>
2021-05-04 00:00:47 -0500

    debuggear aplicacion

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+    let num = 10;
+    debugger;
+    num += 10 ;
+    num = num *3;
+    console.log('INICIO APLICACION');
+    console.log({num});
+    
+  }
   constructor() {}
 
 }
